063ce0356407d5352bab4fd513489bc86d751e66
Ben Martin <bmartin2009@gmail.com>
2022-02-22 21:21:15 -0600

    Some basic file structure population.

---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <header>
+      <h1>
+        Ben Martin Web Design
+      </h1>
+      <h2>
+        Bad at marketing myself. Good at making websites.
+      </h2>
+    </header>
+  )
+}
