583e6f3de6ca89e71cf5533d81c29f9da062cd09
Ben Martin <bmartin2009@gmail.com>
2022-02-23 15:03:37 -0600

    Setting up for use on github.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar.js';
-import Banner from './components/Banner.js'
+import Banner from './components/Banner.js';
+import Body from './components/Body.js';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Banner />
+      <Body />
     </div>
   );
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,13 +2,10 @@ import React from 'react';
 
 export default function Navbar() {
   return (
-    <header>
-      <h1>
-        Ben Martin Web Design
-      </h1>
-      <h2>
-        Bad at marketing myself. Good at making websites.
-      </h2>
+    <header className="jumbotron">
+      <div className="container for-about">
+        <h1>About</h1>
+      </div>
     </header>
   )
 }
